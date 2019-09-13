138a23f1e9e7b60b0f2d56bd96bb16b73df5752c
pemagrg1 <pema.grg1@gmail.com>
2019-09-13 16:03:11 +0545

    adding agglomerative clustering algo and change in code structure

--- SentenceClustering.py
+++ SentenceClustering.py
@@ -5,50 +5,57 @@ from nltk.stem import PorterStemmer
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import AffinityPropagation
+from sklearn.cluster import AgglomerativeClustering
 
 
 
 class SentenceClustering:
-    def __init__(self, **kwargs):
-        self.__dict__.update(kwargs)
+    def __init__(self, sents, nclusters):
+        self.sents = sents
+        self.nclusters = nclusters
 
-    def word_tokenizer(self,text):
-            # tokenizes and stems the text
-            tokens = word_tokenize(text)
-            stemmer = PorterStemmer()
-            tokens = [stemmer.stem(t) for t in tokens if
-                      t not in stopwords.words('english')]
-            return tokens
-
-    def kmeas_clustering(self,sentences, nb_of_clusters=5):
-        tfidf_vectorizer = TfidfVectorizer(tokenizer=self.word_tokenizer,
-                                           stop_words=stopwords.words(
-                                               'english'),
-                                           max_df=0.9,
-                                           min_df=0.1,
-                                           lowercase=True)
-        tfidf_matrix = tfidf_vectorizer.fit_transform(sentences)
-        kmeans = KMeans(n_clusters=nb_of_clusters)
+    def kmeas_clustering(self):
+        tfidf_vectorizer = TfidfVectorizer(stop_words=stopwords.words(
+            'english'),
+            max_df=0.9,
+            min_df=0.1,
+            lowercase=True)
+        tfidf_matrix = tfidf_vectorizer.fit_transform(self.sents)
+        kmeans = KMeans(n_clusters=self.nclusters)
         kmeans.fit(tfidf_matrix)
         clusters = collections.defaultdict(list)
         for i, label in enumerate(kmeans.labels_):
             clusters[label].append(i)
         return dict(clusters)
 
-    def affinity_clustering(self,sentences, nb_of_clusters=5):
-        tfidf_vectorizer = TfidfVectorizer(tokenizer=self.word_tokenizer,
-                                           stop_words=stopwords.words(
+    def affinity_clustering(self):
+        tfidf_vectorizer = TfidfVectorizer(stop_words=stopwords.words(
                                                'english'),
                                            max_df=0.9,
                                            min_df=0.1,
                                            lowercase=True)
-        tf_idf_matrix = tfidf_vectorizer.fit_transform(sentences)
+        tf_idf_matrix = tfidf_vectorizer.fit_transform(self.sents)
         similarity_matrix = (tf_idf_matrix * tf_idf_matrix.T).A
         affinity_propagation = AffinityPropagation(affinity="precomputed",
                                                    damping=0.5)
         affinity_propagation.fit(similarity_matrix)
         clusters = collections.defaultdict(list)
         for i, label in enumerate(affinity_propagation.labels_):
+            clusters[label].append(i)
+        return dict(clusters)
+
+    def Agglomerative_clustering(self):
+        tfidf_vectorizer = TfidfVectorizer(stop_words=stopwords.words(
+            'english'),
+            max_df=0.9,
+            min_df=0.1,
+            lowercase=True)
+        tf_idf_matrix = tfidf_vectorizer.fit_transform(self.sents)
+        similarity_matrix = (tf_idf_matrix * tf_idf_matrix.T).A
+        agglomerativeclustering = AgglomerativeClustering()
+        agglomerativeclustering.fit(similarity_matrix)
+        clusters = collections.defaultdict(list)
+        for i, label in enumerate(agglomerativeclustering.labels_):
             clusters[label].append(i)
         return dict(clusters)
 
