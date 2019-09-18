831dd2611e8ae87fb29f2f5c0f19c98d678bc8f2
pemagrg1 <pema.grg1@gmail.com>
2019-09-18 17:43:22 +0545

    cleaning the code

--- SentenceClustering.py
+++ SentenceClustering.py
@@ -1,14 +1,8 @@
 import collections
-from nltk import word_tokenize
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
 from sklearn.cluster import KMeans
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import AffinityPropagation
 from sklearn.cluster import AgglomerativeClustering
-import matplotlib.pyplot as plt
-from sklearn.manifold import TSNE
-import numpy as np
 from cluster_visualization import ClusterVisualization
 
 class SentenceClustering:
@@ -22,11 +16,13 @@ class SentenceClustering:
         tfidf_matrix = tfidf_vectorizer.fit_transform(self.sents)
         kmeans = KMeans(n_clusters=self.nclusters)
         kmeans.fit(tfidf_matrix)
+
+        clus_viz = ClusterVisualization(tfidf_matrix)
+        clus_viz.cluster_visualization_tsne()
+
         clusters = collections.defaultdict(list)
         for i, label in enumerate(kmeans.labels_):
             clusters[label].append(i)
-        clus_viz = ClusterVisualization(tfidf_matrix)
-        clus_viz.cluster_visualization_tsne()
         return dict(clusters)
 
     def affinity_clustering(self):
@@ -36,6 +32,7 @@ class SentenceClustering:
         affinity_propagation = AffinityPropagation(affinity="precomputed",
                                                    damping=0.5)
         affinity_propagation.fit(similarity_matrix)
+
         clusters = collections.defaultdict(list)
         for i, label in enumerate(affinity_propagation.labels_):
             clusters[label].append(i)
@@ -47,6 +44,7 @@ class SentenceClustering:
         similarity_matrix = (tf_idf_matrix * tf_idf_matrix.T).A
         agglomerativeclustering = AgglomerativeClustering()
         agglomerativeclustering.fit(similarity_matrix)
+
         clusters = collections.defaultdict(list)
         for i, label in enumerate(agglomerativeclustering.labels_):
             clusters[label].append(i)
